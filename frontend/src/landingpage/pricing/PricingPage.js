2f0824d51d7f744c2104047cd4b6782b55f27185
Yash <yashprogrammers@gmail.com>
2025-11-22 07:20:31 +0530

    design about, pricing, products & support pages

--- frontend/src/landingpage/pricing/PricingPage.js
+++ frontend/src/landingpage/pricing/PricingPage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from './Hero'
 import Brokerage from './Brokerage'
+import OpenAccount from '../OpenAccount'
 
 function PricingPage() {
   return (
     <>
     <Hero/>
+    <OpenAccount/>
     <Brokerage/>
     </>
   )
